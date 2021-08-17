4bf4645c6edd41c3e705d60fdd0c2dee31c42697
Yuno1234 <61713594+Yuno1234@users.noreply.github.com>
2021-08-17 12:30:40 +1200

    end of workshop 6

--- run.py
+++ run.py
@@ -11,39 +11,47 @@ app = Flask(__name__)
 
 KCALDB = 'kcal-per-person.db'
 
-# def getColor(kcal):
-#     if kcal > 3500 and kcal < 4000:
-#         return 
-#     elif kcal > 3000 and kcal < 3499:
-#         return
-#     elif kcal > 2500 and kcal < 2999:
-#         return
-#     elif kcal > 2000 and kcal < 2499:
-#         return
-#     elif kcal > 1500 and kcal < 1999:
-#         return
-#     elif kcal > 0 and kcal < 1499:
-#         return
-#     else
-#         return
+def getColor(kcal):
+    if kcal > 3500 and kcal < 4000:
+        return "#FF0000"
+    elif kcal > 3000 and kcal < 3499:
+        return "#CC0000"
+    elif kcal > 2500 and kcal < 2999:
+        return "#990000"
+    elif kcal > 2000 and kcal < 2499:
+        return "#550000"
+    elif kcal > 1500 and kcal < 1999:
+        return "#330000"
+    elif kcal > 0 and kcal < 1499:
+        return "#110000"
+    else:
+        return "#000000"
 
-country = {
-  'Afghanistan': '#FF2211'
-}
+country = {}
 
-@app.route('/', methods=['get'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
   con = sqlite3.connect(KCALDB)
   years = []
+  year = 1961
   cur = con.execute('SELECT year FROM food_supply_kcal WHERE entity = "Japan"')
   for row in cur:
     years.append(list(row))
 
-  countries = []
-  cur = con.execute('SELECT entity, kcal FROM food_supply_kcal WHERE year = "2017"')
-  for row in cur:
-    year.append(list(row))
+  if request.method == 'POST':
+    year = request.form['years']
+    print(year)
+    cur = con.execute('SELECT entity, kcal FROM food_supply_kcal WHERE year = ?', (year,))
+    for row in cur:
+      country[row[0]] = getColor(row[1])
 
-  print(request.form)
+  if request.method == 'GET':
+    cur = con.execute('SELECT entity, kcal FROM food_supply_kcal WHERE year = "1961"')
+    for row in cur:
+      country[row[0]] = getColor(row[1])
+    
+
+
+
   con.close()
-  return render_template('index.html', years=years, country=country)
+  return render_template('index.html', years=years, country=country, selected=int(year))
